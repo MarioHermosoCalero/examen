1fa39b3e469d064041bc9ce1758e209879b106fa
Mario Hermoso Calero <mario.hermosocalero@educa.madrid.org>
2024-12-29 11:45:54 +0000

    Se elimina el sitio web que se había quedado por error.

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,6 @@
 
     'author': 'XXXXXXX',
     'website': 'XXXXXXXXXX',
-    # 'website': "https://site.educa.madrid.org/ies.alonsodeavellan.alcala/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/17.0/odoo/addons/base/data/ir_module_category_data.xml
